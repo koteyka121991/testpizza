8bb3380173f7d4031bb4f1d7654667ae01eae10b
koteyka121991 <106138744+koteyka121991@users.noreply.github.com>
2022-12-18 19:29:22 +0300

    installed redux

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-    Link,
-    BrowserRouter,
-} from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // render оображение, отрисовка html взятый из какого либо компонента
 // ctrl +j открытие терминала
 // в js запускать html нельзя
 // JSX – это расширение синтаксиса JavaScript, которое во время компиляции транслируется в обычный JavaScript.
 // npm install sass установка sass
+// slice куски с логикой коорая будет обрабатыать хранилище раньше это называлось reduce
 root.render(
-    <BrowserRouter >
-        <App />
-    </BrowserRouter>
-
-
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
-
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from '@reduxjs/toolkit'
+// переменая store это редакс хранилище
+export const store = configureStore({
+  reducer: {},
+})
